1fcd570e8064eac5bb0cf19d5d94f4397a05f125
jayagullano <jayagullano@gmail.com>
2021-06-22 18:04:59 -0400

    Added next card handler to test cycling cards.
    Added new button to handle the change of card use an iterator.

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/App.css';
 import * as generator from './deckGenerator/deckGeneratorFunction';
+import Card from './card/card';
 
 function App() {
 
   let deck = generator.generate();
 
+  let currentDeck = Array.from(deck); //Creates new deck from the initial deck
+  currentDeck = currentDeck[Symbol.iterator](); //Creates iterator for copied deck
+  currentDeck.next();
+
+  let [card, setCard] = useState(currentDeck.next().value);
+  let counter = 1;
+
+  function nextCard() {
+    console.log(counter, ':', card);
+    setCard(currentDeck.next().value);
+    counter = counter + 1;
+  }
+
+
+  function restart(){
+    currentDeck = Array.from(deck); //Creates new deck from the initial deck
+    currentDeck = currentDeck[Symbol.iterator](); //Creates iterator for copied deck
+    currentDeck.next();
+    setCard(currentDeck.next().value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-
-      {deck.map(elem => {
-        return <p>{elem[1]} of {elem[0]}</p>;
-      })}
+      <div>
+        <Card suit={card[1]} number={card[0]}/>
+      </div>
+      <br></br>
+      <button onClick={nextCard}>Next Card</button>
+      <br></br>
+      <button onClick={restart}>Restart</button>
       </header>
     </div>
   );
