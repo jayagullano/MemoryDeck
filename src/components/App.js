26ea0ee371095cfc0df1e42f514f16835a8bf66e
jayagullano <jayagullano@gmail.com>
2021-06-29 21:32:06 -0400

    Reformatted App.js to new memorizationMode.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,16 @@
-import React, { useState } from 'react';
-import './css/App.css';
+import React from 'react';
 import * as generator from './deckGenerator/deckGeneratorFunction';
-import Card from './card/playingCard';
+import MemorizationMode from './modes/memorizationMode';
 
+
+//Deck generated from deckGenerator
 let deck = generator.generate();
 
 function App() {
   
-  let [index, setIndex] = useState(0);
-  let [card, setCard] = useState(deck[index]);
-
-  function nextCard() {
-
-    if(index < (deck.length-1)){ //Stops incrementing index after last card
-      let nextIndex = index + 1;
-      console.log(deck[index]);    //Displays current card
-      setIndex(nextIndex);
-      setCard(deck[nextIndex]);
-    }
-  }
-
-  function restart(){
-    setIndex(0);
-    setCard(deck[0]);
-  }
-
   return (
-    <div className="App">
-
-      <div className="App-header">
-        <h2>{'Card : ' + (index+1)}</h2>
-      <div>
-        <Card suit={card[0]} number={card[1]} image={card[2]}/>
-      </div>
-      <br></br>
-      <button onClick={nextCard}>Next Card</button>
-      <br></br>
-      <button onClick={restart}>Restart</button>
-      </div>
+    <div>
+      <MemorizationMode deck={deck} />
     </div>
   );
 }
---
+++ src/components/modes/memorizationMode.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core';
+import Card from '../card/playingCard';
+
+const useStyles = makeStyles((theme) => ({
+    App: {
+        textAlign: 'center'
+    },
+    AppHeader: {
+        backgroundColor: '#282c34',
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: `calc(10px + 2vmin)`,
+        color: 'white',
+      },
+}));
+
+function MemorizationMode(props) {
+
+  let classes = useStyles();
+  
+  let [index, setIndex] = useState(0);
+  let [card, setCard] = useState(props.deck[index]);
+
+  function nextCard() {
+
+    if(index < (props.deck.length-1)){ //Stops incrementing index after last card
+      let nextIndex = index + 1;
+      console.log(props.deck[index]);    //Displays current card
+      setIndex(nextIndex);
+      setCard(props.deck[nextIndex]);
+    }
+  }
+
+  function restart(){
+    setIndex(0);
+    setCard(props.deck[0]);
+  }
+
+  return (
+    <div className={classes.App}>
+      <div className={classes.AppHeader}>
+        <h2>{'Card : ' + (index+1)}</h2>
+      <div>
+        <Card suit={card[0]} number={card[1]} image={card[2]}/>
+      </div>
+      <br></br>
+      <button onClick={nextCard}>Next Card</button>
+      <br></br>
+      <button onClick={restart}>Restart</button>
+      </div>
+    </div>
+  );
+}
+
+export default MemorizationMode;
